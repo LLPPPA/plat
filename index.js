7ae768d212578228410ae03f9bf3206fdb062ba4
LLPPPA <51243034+LLPPPA@users.noreply.github.com>
2019-06-01 14:33:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const {Client, Attachment} = require('discord.js');
+const bot = new Client();
 
 const token = 'NTU5MDIwNzcwMDYwMjcxNjYw.XPIRxg.12jBlrJe3wW6eYwKtieCqGyLf88';
+
+const PREFIX = '&'
 
 bot.on('ready', () =>{
     console.log('what the f');
@@ -9,4 +11,16 @@ bot.on('ready', () =>{
 
 bot.login(token);
 
+bot.on('message', msg=> {
+    
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'explosion':
+            const attachment = new Attachment('https://i.ytimg.com/vi/O4tbOvKwZUw/maxresdefault.jpg')
+            message.channel.send(message.author, attachment);
+            break;
+             
+    }
+    
 
