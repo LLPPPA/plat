6069d463c5dbe07d5bb3d73269975147f384d1ae
LLPPPA <51243034+LLPPPA@users.noreply.github.com>
2019-06-01 15:32:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready', () =>{
 
 bot.on('guildMemberAdd', member =>{
 
-    const channel = member.guild.channels.find(channel => channel.name === "新進成員");
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
     if(!channel) return;
 
     channel.send(`歡迎加入惠惠教! ${member}`)
