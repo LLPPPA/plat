be30e7e28e0f221a3d3c1abd5254951dd068f375
LLPPPA <51243034+LLPPPA@users.noreply.github.com>
2019-06-01 13:58:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NTU5MDIwNzcwMDYwMjcxNjYw.XPIRxg.12jBlrJe3wW6eYwKtieCqGyLf88';
+
+bot.on('ready', () =>{
+    console.log('what the f');
+})
+
+bot.login(token);
+
+
